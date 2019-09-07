76299c56f38dd5dd2fd6b4733357056f56e1ee72
靓仔 <970767662@qq.com>
2019-09-07 16:54:51 +0800

    05-机票列表页分页功能实现

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,76 +1,101 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <FlightsListHead/>
-                
-                
-                <!-- 航班信息 -->
-                <FlightsItem 
-                v-for="(item,index) in dataList"
-                :key="index"
-                :data="item"
-                />
-            </div>
+        <!-- 航班头部布局 -->
+        <FlightsListHead />
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 航班信息 -->
+        <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
+
+        <!-- 分页 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="pageNum"
+          :page-sizes="[5, 10, 15, 20]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          style="margin-left:100px"
+        ></el-pagination>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
 import moment from "moment";
-import FlightsListHead from '@/components/air/flightsListHead'
-import FlightsItem from '@/components/air/FlightsItem'
+import FlightsListHead from "@/components/air/flightsListHead";
+import FlightsItem from "@/components/air/FlightsItem";
 
 export default {
-    data(){
-        return {
-            flightsData:{},
-            dataList:[]
-        }
+  data() {
+    return {
+      flightsData: {},
+      dataList: [],
+      pageNum: 1,
+      pageSize: 5,
+      total: 0
+    };
+  },
+  methods: {
+    setDataList() {
+      this.dataList = this.flightsData.flights.slice(
+        (this.pageNum - 1) * this.pageSize,
+        this.pageNum * this.pageSize
+      );
     },
-    components:{
-        FlightsListHead,FlightsItem
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.setDataList();
     },
-    mounted(){
-        this.$axios({
-            url:'/airs',
-            params:this.$route.query
-        }).then(res => {
-            console.log(res);
-            
-            this.flightsData = res.data
-            this.dataList = res.data.flights
-        })
+    handleCurrentChange(val) {
+      this.pageNum = val 
+      this.setDataList();
     }
-}
+  },
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  mounted() {
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      console.log(res);
+
+      this.flightsData = res.data;
+      //   this.dataList = res.data.flights;
+      this.setDataList();
+      this.total = res.data.total;
+    });
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
